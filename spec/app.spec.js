089a1b369d807ff77bf9845b2e0e162d263523e5
RyanK79 <Nermal5@msn.com>
2018-03-11 21:08:03 -0400

    added javascript, still trying to figure out these jasmine tests

---
+++ spec/app.spec.js
@@ -0,0 +1,25 @@
+describe ("my modal should open on click", function(){
+    it ("opens when clicked on", function() {
+        element(by.id("myBtn")).click();
+    });
+});
+
+describe ("my carousel rotates pictures when arrow click", function(){
+    it ("rotates when arrow clicked", function(){
+
+    });
+});
+
+describe ("nav menu works when clicked", function(){
+    it ("nav menu appears when clicked on", function(){
+
+    });
+});
+
+describe ("when click on project read more button it expands", function(){
+    it ("when click on read more it expands to give description", function(){
+
+    });
+});
+
+   
